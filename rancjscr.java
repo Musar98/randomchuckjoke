33cf1e6b37ba3f5e620bc771c1bfa16000e0adfe
Musar98 <40075894+Musar98@users.noreply.github.com>
2021-08-02 00:20:54 +0200

    Create rancjscr.java

---
+++ rancjscr.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class javafile1 {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.chucknorris.io/jokes/random"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        String r_b = response.body();
+
+        int y = r_b.indexOf("value");
+        int z = r_b.indexOf('}');
+
+        String sub = r_b.substring(y+8,z-1);
+
+        System.out.println(sub);
+
+    }
+}
