9ff872c87d6c40ab722c800b3692510ef8ae222e
Musar98 <40075894+Musar98@users.noreply.github.com>
2021-08-04 01:05:23 +0200

    Update and rename randomchjscr.java to randomjoke.java

--- randomjoke.java
+++ randomjoke.java
@@ -4,7 +4,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class randomchjscr {
+public class randomjoke {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
