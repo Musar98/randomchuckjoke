608448b84141f6dfff635ffec3c47e023bf12da5
Musar98 <40075894+Musar98@users.noreply.github.com>
2021-08-04 01:04:11 +0200

    Update randomchjscr.java

--- randomchjscr.java
+++ randomchjscr.java
@@ -4,7 +4,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class javafile1 {
+public class randomchjscr {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
